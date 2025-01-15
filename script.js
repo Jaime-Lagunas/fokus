98c605d5061e7c9eb9dccc284bdc8d131c60bee1
Jaime-Lagunas <139659104+Jaime-Lagunas@users.noreply.github.com>
2025-01-15 00:27:01 -0600

    cambio de contexto funciones

---
+++ script.js
@@ -0,0 +1,27 @@
+const html = document.querySelector('html')
+const botonCorto = document.querySelector('.app__card-button--corto')
+const botonEnfoque = document.querySelector ('.app__card-button--enfoque')
+const botonLargo = document.querySelector ('.app__card-button--largo')
+const banner = document.querySelector('.app__image')
+
+botonCorto.addEventListener ('click', () => {
+    cambiarContecto('descanso-corto')
+    
+})
+
+botonEnfoque.addEventListener('click', () => {
+    cambiarContecto('enfoque')
+})
+
+botonLargo.addEventListener('click', () => {
+    cambiarContecto('descanso-largo')
+    
+})
+
+``
+
+function cambiarContecto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src',`/imagenes/${contexto}.png`)
+}
+
