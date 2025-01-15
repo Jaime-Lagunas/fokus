34039638224a347ea0aedc7b604c42307f125538
Jaime-Lagunas <139659104+Jaime-Lagunas@users.noreply.github.com>
2025-01-15 15:44:49 -0600

    cambio de colores, texto, video inverso al click

--- script.js
+++ script.js
@@ -3,24 +3,36 @@ const botonCorto = document.querySelector('.app__card-button--corto')
 const botonEnfoque = document.querySelector ('.app__card-button--enfoque')
 const botonLargo = document.querySelector ('.app__card-button--largo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botones = document.querySelectorAll('.app__card-button')
+
+
+
 
 botonCorto.addEventListener ('click', () => {
     cambiarContecto('descanso-corto')
-    
+    botonCorto.classList.add('active')
 })
 
 botonEnfoque.addEventListener('click', () => {
     cambiarContecto('enfoque')
+    botonEnfoque.classList.add('active')
 })
 
 botonLargo.addEventListener('click', () => {
     cambiarContecto('descanso-largo')
-    
+    botonLargo.classList.add('active')
 })
 
 
 
 function cambiarContecto(contexto){
+
+    botones.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
+
+
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src',`/imagenes/${contexto}.png`)
 
@@ -30,21 +42,19 @@ function cambiarContecto(contexto){
              Optimiza tu productividad,<br>
                 <strong class="app__title-strong">sumérgete en lo que importa.</strong>
             `
-        
             break;
         case "descanso-corto":
             titulo.innerHTML=`
             ¿Qué tal tomar un respiro? <br>
-                <strong class="app__title-strong">¡Haz una pausa corta!</strong>
+                <strong class="app__title-strong">¡Haz una pausa más corta!</strong>
             `
-            break
+            break;
         case "descanso-largo":
             titulo.innerHTML = `
             Hora de volver a la superficie <br>
                 <strong class="app__title-strong">Haz una pausa larga.</strong>
             
             `        
-    
         default:
             break;
     }
