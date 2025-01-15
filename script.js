4edcb4b748c9e3e9e0cffa8225b93b4a680c86e2
Jaime-Lagunas <139659104+Jaime-Lagunas@users.noreply.github.com>
2025-01-15 14:08:12 -0600

    Cambio de Texto titulo.innerHTML

--- script.js
+++ script.js
@@ -18,10 +18,35 @@ botonLargo.addEventListener('click', () => {
     
 })
 
-``
+
 
 function cambiarContecto(contexto){
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src',`/imagenes/${contexto}.png`)
+
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML=`
+             Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>
+            `
+        
+            break;
+        case "descanso-corto":
+            titulo.innerHTML=`
+            ¿Qué tal tomar un respiro? <br>
+                <strong class="app__title-strong">¡Haz una pausa corta!</strong>
+            `
+            break
+        case "descanso-largo":
+            titulo.innerHTML = `
+            Hora de volver a la superficie <br>
+                <strong class="app__title-strong">Haz una pausa larga.</strong>
+            
+            `        
+    
+        default:
+            break;
+    }
 }
 
